1aa20cb1644588948a57a734eca0b18d42b5677c
Httpsnazrin <147836567+Httpsnazrin@users.noreply.github.com>
2023-11-24 10:53:21 +0400

    Add files via upload

---
+++ homewrok9.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LengthOfLastWord
+            //string s = Console.ReadLine();
+            //int result = LengthOfLastWord(s);
+
+            //Console.WriteLine($"Length of the last word: {result}");
+            #endregion
+            #region Reverse Words
+            //string s = Console.ReadLine();
+            //Console.WriteLine(ReverseWordsOrder(s));
+
+            #endregion
+        }
+        #region Reverse Words
+        //public static string ReverseWordsOrder(string s)
+        //{
+        //    string[] words = s.Split(' ');
+        //    //Array.Reverse(words);
+        //    //return string.Join(" ", words);
+           
+        //    string reversedString = "";
+
+        //    for (int i = words.Length - 1; i >= 0; i--)
+        //    {
+        //        reversedString += words[i];
+
+        //        if (i > 0)
+        //        {
+        //            reversedString += " ";
+        //        }
+        //    }
+
+        //    return reversedString;
+        //}
+        #endregion
+        #region LengthOfLastWord
+        //public static int LengthOfLastWord(string s)
+        //{
+        //    int length = 0;
+
+        //    int i = s.Length - 1;
+
+
+        //    while (i >= 0 && s[i] == ' ')
+        //    {
+        //        i--;
+
+        //    }
+        //    while (i >= 0 && s[i] != ' ')
+        //    {
+        //        length++;
+        //        i--;
+        //    }
+        //    return length;
+        // }
+        #endregion
+
+    }
+}
